17befb5c1d5b22f128aa9e2b1945488df5dffc6d
adnanezaki <adnane.zaki-etu@etu.univh2c.ma>
2024-07-27 19:01:21 +0100

    L'instanciation du projet

---
+++ src/test/java/com/adnane/spring_ai_rag/SpringAiRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adnane.spring_ai_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
